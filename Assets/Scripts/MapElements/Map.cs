4fa58cfe3c9d35c595c9198f2aa026a5c1d4ec71
Tong <jonny4th@gmail.com>
2023-05-30 23:11:29 +0700

    Mock character

--- Assets/Scripts/MapElements/Map.cs
+++ Assets/Scripts/MapElements/Map.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Map : MonoBehaviour
@@ -10,21 +10,22 @@ public class Map : MonoBehaviour
     [SerializeField]
     Vector2Int mapSize;
 
-    List<Transform> mapCoordinates = new();
+    public List<Transform> mapCoordinates => GetComponentsInChildren<Tile>().ToList().Select(x => x.transform).ToList();
 
     public void GenerateMap()
     {
         ClearMap();
 
-        float startX = (1 - mapSize.x)/2;
-        float startY = (1 - mapSize.y)/2;
+        float startX = (1 - mapSize.x) / 2;
+        float startY = (1 - mapSize.y) / 2;
+        float startZ = -0.125f;
 
         for(int i = 0; i < mapSize.x; i++)
         {
             for(int j = 0; j < mapSize.y; j++)
             {
-                var pos = new Vector3(i + startX, 0, j + startY);
-                mapCoordinates.Add(Instantiate(prototypeTile, pos, Quaternion.identity, transform).transform);
+                var pos = new Vector3(i + startX, startZ, j + startY);
+                Instantiate(prototypeTile, pos, Quaternion.identity, transform);
             }
         }
     }
